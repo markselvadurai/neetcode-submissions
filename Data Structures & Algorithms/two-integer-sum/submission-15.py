95f9dfcf1f867be80ca0e7944d4c38cef0f80c7a
markselvadurai <113148945+markselvadurai@users.noreply.github.com>
2026-08-18 20:21:33 -0400

    Add: two-integer-sum - submission-15

---
+++ Data Structures & Algorithms/two-integer-sum/submission-15.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        store = {}
+        for index, num in enumerate(nums):
+            if target - num in store:
+                return[store[target-num],index]
+            store[num] = index
